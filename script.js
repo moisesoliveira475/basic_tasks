d22ec2b8a6b82ad890b31c8fcf081b887aa7d4ea
moisesoliveira475 <moises475oliveira@gmail.com>
2023-08-05 16:24:27 -0300

    addition of animation for adding and removing tasks and code modularization

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
 let taskCount = 0;
 
-function addTask() {
-    const taskInput = document.getElementById(`task-input`);
-    const taskList = document.getElementById(`task-list`);
+// Função para criar elementos
+function createElement(tagName, textContent, className, eventListener) {
+    const element = document.createElement(tagName);
+    if (textContent) {
+        element.textContent = textContent;
+    }
+    if (className) {
+        element.classList.add(className);
+    }
+    if (eventListener) {
+        element.addEventListener(`click`, eventListener);
+    }
+    return element
+}
 
-    if (taskInput.value.trim() !== ``) {
-        taskCount ++;
-        const taskListItem = document.createElement(`div`)
-        taskListItem.classList.add(`task-list-item`);
-        taskListItem.textContent = taskInput.value;
-        taskList.appendChild(taskListItem)
+const taskInput = document.getElementById(`task-input`);
+
+// Função para criar item na lista de tarefas
+function createTaskListItem(taskInputValue) {
+    const taskListItem = createElement(`div`, taskInputValue, `task-list-item`);
+    const removeButton = createElement(`button`, `Remover`, `btn-rm-tasks`, () => {
+    removeTaskListItem(taskListItem);
+});
+    taskListItem.appendChild(removeButton);
+    return taskListItem
+}
+// Função para remover item da lista de tarefas
+function removeTaskListItem(taskListItem) {
+    taskListItem.classList.add(`removing`);
+    setTimeout(() => {
+        taskListItem.remove();
+        taskCount--;
         updateTaskCount();
-        taskInput.value = ``;
-        console.log(taskList)
+        if (taskCount === 0) {
+            taskListItem.classList.remove(`removing`);
+        }
+    }, 300);
+    if (taskCount === 1) {
+        taskListItem.classList.add(`removing`);
     }
 }
+// Função para adicionar tarefa
+function addTask() {
+    const taskList = document.getElementById(`task-list`);
+    if (taskInput.value.trim() !== ``) {
+        taskCount++;
+        const taskListItem = createTaskListItem(taskInput.value);
+        taskList.appendChild(taskListItem);
+        updateTaskCount();
+        taskInput.value = ``;
+    }
+}
+// função para atualização do contador de tarefas
 function updateTaskCount() {
     const taskCountElement = document.getElementById(`task-count`);
     taskCountElement.textContent = taskCount;
 }
-
-const taskList = document.querySelector(`div.task-list-item`);
-
-function rmTasks() {
-    taskList.value = ``
-}
+// 
+taskInput.addEventListener(`keydown`, (event) => {
+    if (event.key === `Enter` && taskInput.value.trim() !== ``) {
+        addTask()
+    }
+});
