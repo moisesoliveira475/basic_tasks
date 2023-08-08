3f9c8c887720a9d8b54be007bfafae6930052430
moisesoliveira475 <moises475oliveira@gmail.com>
2023-08-07 22:55:17 -0300

    add completedTaskList, finish_btn & docs functions

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+/**
+ * @file This file contains functions to manage a task list.
+ * @version 1.0.0
+ */
+
+/**
+ * @type {number} taskCount - The number of tasks in the list
+ */
+
 let taskCount = 0;
 
-// Função para criar elementos
+/**
+ * Creates an HTML element with the specified tag name, text content, class name, and event listener.
+ * @param {string} tagName - The tag name of the element to create
+ * @param {string} [textContent] - The text content of the element to create
+ * @param {string} [className] - The class name of the element to create
+ * @param {function} [eventListener] - The event listener function for the element to create
+ * @returns {HTMLElement} The created HTML element
+ */
+
 function createElement(tagName, textContent, className, eventListener) {
     const element = document.createElement(tagName);
     if (textContent) {
@@ -15,18 +32,51 @@ function createElement(tagName, textContent, className, eventListener) {
     return element
 }
 
+/**
+ * The input element for adding tasks
+ * @type {HTMLInputElement}
+ */
+
 const taskInput = document.getElementById(`task-input`);
 
-// Função para criar item na lista de tarefas
+/**
+ * Creates a task list item with the specified task input value.
+ * @param {string} taskInputValue - The value of the task input
+ * @returns {HTMLElement} The created task list item
+ */
+
 function createTaskListItem(taskInputValue) {
     const taskListItem = createElement(`div`, taskInputValue, `task-list-item`);
     const removeButton = createElement(`button`, `Remover`, `btn-rm-tasks`, () => {
     removeTaskListItem(taskListItem);
 });
+    const finishButton = createElement(`button`, `Concluir`, `btn-finish-task`, () => {
+    completeTaskListItem(taskListItem);
+    });
+
+    taskListItem.appendChild(finishButton);
     taskListItem.appendChild(removeButton);
     return taskListItem
-}
-// Função para remover item da lista de tarefas
+};
+
+/**
+ * Completes the specified task list item.
+ * @param {HTMLElement} taskListItem - The task list item to complete
+ */
+
+function completeTaskListItem(taskListItem) {
+    taskListItem.classList.add(`completed`);
+    const completedTaskList = document.getElementById(`completed-task-list`);
+    completedTaskList.appendChild(taskListItem);
+    taskCount--;
+    updateTaskCount();
+};
+
+/**
+ * Removes the specified task list item.
+ * @param {HTMLElement} taskListItem - The task list item to remove
+ */
+
 function removeTaskListItem(taskListItem) {
     taskListItem.classList.add(`removing`);
     setTimeout(() => {
@@ -41,7 +91,11 @@ function removeTaskListItem(taskListItem) {
         taskListItem.classList.add(`removing`);
     }
 }
-// Função para adicionar tarefa
+
+/**
+ * Adds a task to the task list.
+ */
+
 function addTask() {
     const taskList = document.getElementById(`task-list`);
     if (taskInput.value.trim() !== ``) {
@@ -52,12 +106,16 @@ function addTask() {
         taskInput.value = ``;
     }
 }
-// função para atualização do contador de tarefas
+
+/**
+ * Updates the task count element with the current task count.
+ */
+
 function updateTaskCount() {
     const taskCountElement = document.getElementById(`task-count`);
     taskCountElement.textContent = taskCount;
 }
-// 
+
 taskInput.addEventListener(`keydown`, (event) => {
     if (event.key === `Enter` && taskInput.value.trim() !== ``) {
         addTask()
